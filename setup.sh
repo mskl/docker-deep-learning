504890aba8d7d6b25a717625bd253307587bf248
Matyáš Skalický <matyaskalicky@gmail.com>
2020-09-18 09:35:55 +0200

    Added setup

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Add the NVIDIA drivers repo
+# https://docs.nvidia.com/datacenter/tesla/tesla-installation-notes/index.html
+sudo apt-get install linux-headers-$(uname -r)
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+
+# Install drivers, docker and docker compose
+sudo apt-get update
+sudo apt-get -y install cuda-drivers docker docker.io  python3-pip
+sudo pip3 install docker-compose
+
+export PATH=/usr/local/cuda-11.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-11.0/lib64 ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+sudo /usr/bin/nvidia-persistenced --verbose
+
+# Restart the docker service for the changes to take effect
+sudo systemctl start docker
+sudo systemctl enable docker
